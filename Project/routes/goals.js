0d6d7332c29b6311fdedc73012548b07564e08c5
Ezana Bahta <ezanab2@gmail.com>
2018-10-04 09:03:14 -0400

    CRUD on new.hbs

--- Project/routes/goals.js
+++ Project/routes/goals.js
@@ -3,6 +3,10 @@ const router = express.Router(); // initializes the express with Router method
 const Goals = require('../models/Goals')
 //homepage.js
 
+router.get('/', (req, res) => {
+    res.render(`goals/new`)
+})
+
 // path is home/login
 router.post('/new', (req, res) => { // express.Router.get('browser path, (Server Request, Server Response))
     // home page
